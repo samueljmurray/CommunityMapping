56ca56c596f7a4f6d84cebb4f5f3269c10738117
Samuel Murray <samueljmurray@googlemail.com>
2014-03-17 23:52:55 +0000

    Added server file

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    routes = require('./app/routes'),
+    exphbs = require('express3-handlebars'),
+    mongoose = require('mongoose'),
+    seeder = require('./app/seeder'),
+    app = express();
+
+app.set('port', process.env.PORT || 3300);
+app.set('views', __dirname + '/views');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: app.get('views') + '/layouts'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('potato-eleven-origin-delta'));
+app.use(app.router);
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+// DEV
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+// DB connect
+mongoose.connect('mongodb://localhost/CommunityMapping');
+mongoose.connection.on('open', function() {
+    console.log("Connected to Mongoose...");
+});
+
+// Routes
+routes.initialize(app);
+
+// Start server
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Server up: http://localhost:' + app.get('port'));
+});
